b862f15432681ebccc3aaf39a1cbb607bddc0a31
Alvin Acosta <alvin.acosta@movon.com.ph>
2022-02-02 16:07:09 +0800

    feature/download-reports/build2

--- src/config/index.js
+++ src/config/index.js
@@ -9,7 +9,7 @@ export const config = {
     build: "1.0.0(2)",
   },
   environment: "",
-  changeLogs: `feature/media-queries/build2`,
+  changeLogs: `feature/download-reports/build2`,
 };
 
 export const ERROR_CODES = {
